b583220795901a015b6961f157610d35b887bbd1
xxxginxxx <xxxginxxx1213@gmail.com>
2017-05-04 23:47:55 +0900

    Merge remote-tracking branch 'refs/remotes/origin/sub_1' into Takahashi
    # Conflicts:
    #	CESA2017/.vs/CESA2017/v14/.suo
    #	CESA2017/Assets/Scene/Test_Takahashi.unity
    #	CESA2017/Library/CurrentLayout.dwlt
    #	CESA2017/Library/InspectorExpandedItems.asset
    #	CESA2017/Library/ScriptAssemblies/Assembly-CSharp.dll
    #	CESA2017/Library/ScriptAssemblies/Assembly-CSharp.dll.mdb
    #	CESA2017/Library/assetDatabase3
    #	CESA2017/Library/metadata/2a/2a4a5c54f8d16da439766f382f1053c7

---
+++ CESA2017/Assets/Script/DeleteArea.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DeleteArea : MonoBehaviour {
+
+    void OnCollisionEnter(Collision hit)
+    {
+        Destroy(hit.gameObject);
+    }
+}
