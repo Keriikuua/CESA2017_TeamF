1729c8f1c177c531560fc6e11ff5a507e7f9c9b3
xxxginxxx <xxxginxxx1213@gmail.com>
2017-05-18 15:21:25 +0900

    2017_0518_下画面

--- CESA2017/Assets/Script/Mold.cs
+++ CESA2017/Assets/Script/Mold.cs
@@ -5,16 +5,138 @@ using UnityEngine;
 // 型クラス
 public class Mold : MonoBehaviour {
 
+    // 
+    public enum MoldMode
+    {
+        None = 0,
+        Down,
+        Stop,
+        Up,
+    }
+
     [SerializeField,Header("型のタイプ")]
     Type.Chara MoldType;
 
-	// Use this for initialization
-	void Start () {
-		
-	}
+    [SerializeField, Header("型の落下速度")]
+    float fSpeed;
+
+    [SerializeField, Header("中心")]
+    GameObject obj;
+
+    [SerializeField,Header("対応キー")]
+    string Botton;
+
+    [SerializeField, Header("BAD判定ゾーン"),Range(0.0f,0.25f)]
+    float BadZone;
+
+    [SerializeField, Header("GOOD判定ゾーン"), Range(0.0f, 0.25f)]
+    float GoodZone;
+
+    [SerializeField, Header("フィーバーゲージ")]
+    Fever fever;
+
+    private Vector3 InitPos;
+    private MoldMode mode;
+    private float BestZone;
+    private BoxCollider Collider;
+    private float distance = 100f;
+
+    // Use this for initialization
+    void Start () {
+        mode = MoldMode.None;
+        InitPos = transform.position;
+        Collider = transform.GetComponent<BoxCollider>();
+        BadZone = BadZone * Collider.size.x;
+        GoodZone = GoodZone * Collider.size.x;
+        BestZone = Collider.size.x - (BadZone + GoodZone);
+    }
 	
 	// Update is called once per frame
-	void Update () {
-		
-	}
+	void Update ()
+    {
+        switch (mode)
+        {
+            case MoldMode.None:
+                // キーで挟む
+                if (Input.GetKeyDown(Botton))
+                    mode = MoldMode.Down;
+
+                // 左クリックを取得
+                if (Input.GetMouseButtonDown(0))
+                {
+                    // クリックしたスクリーン座標をrayに変換
+                    Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+                    // Rayの当たったオブジェクトの情報を格納する
+                    RaycastHit hit = new RaycastHit();
+                    // オブジェクトにrayが当たった時
+                    if (Physics.Raycast(ray, out hit, distance))
+                    {
+                        // rayが当たったオブジェクトの名前を取得
+                        string objectName = hit.collider.gameObject.name;
+                        if(objectName == gameObject.name)
+                            mode = MoldMode.Down;
+                    }
+                }
+                break;
+
+            case MoldMode.Down:
+                if (transform.position.y > obj.transform.position.y + (transform.localScale.y * 1.5f))
+                    transform.position -= new Vector3(0, fSpeed, 0);
+                else
+                    mode = MoldMode.Up;
+                break;
+
+            case MoldMode.Stop:
+                break;
+
+            case MoldMode.Up:
+
+                if (transform.position.y < InitPos.y)
+                    transform.position += new Vector3(0, fSpeed/10.0f, 0);
+                else
+                    mode = MoldMode.None;
+
+                break;
+        }
+    }
+
+    public int HitCollider(Transform hitObj)
+    {
+        if (fever.GetFeverMode())   // Fever中なら
+        {
+            fever.PlusGauge(2);
+            return 1;
+        }
+
+        float PosX = transform.position.x;
+        float ScaleX = Collider.size.x;
+        float HitZone = Mathf.Abs(hitObj.position.x - PosX);
+
+        // Best判定
+        if ((ScaleX - BadZone) - GoodZone > HitZone && (ScaleX - BadZone) - GoodZone - BestZone < HitZone)
+        {
+            fever.PlusGauge(2);
+            return 1;
+        }
+
+        // Good判定
+        if ((ScaleX - BadZone) > HitZone && (ScaleX - BadZone) - GoodZone <HitZone)
+        {   
+            fever.PlusGauge(1);
+            return 2;
+        }
+
+        // Bad判定
+        //if (ScaleX > HitZone && (ScaleX - BadZone) < HitZone)
+        //{
+        fever.PlusGauge(0);
+        return 3;
+        //}
+        //return 0;
+    }
+
+    public Type.Chara GetType()
+    {
+        return MoldType;
+    }
 }
