23946cf3de2fee88932e95a5824ffd3368eb48ef
xxxginxxx <xxxginxxx1213@gmail.com>
2017-06-07 08:47:07 +0900

    0607_TestCommit

---
+++ CESA2017/Assets/Script/TitleElemental.cs
@@ -0,0 +1,57 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TitleElemental : MonoBehaviour {
+
+    [SerializeField, Header("素が流れる速さ")]
+    Vector3 Speed;
+
+    bool bDirect;
+    Vector3 InitPos;
+
+    // Use this for initialization
+    void Start () {
+        bDirect = false;
+        InitPos = transform.position;
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+    }
+
+    // 当たってる間呼ばれ続ける
+    void OnCollisionStay(Collision hit)
+    {
+        if (hit.gameObject.tag == "Convair")    //　コンベアに乗ってて止まってない時
+        {
+            if (!bDirect)
+            {
+                transform.position += Speed;
+
+                if(transform.position.x < InitPos.x - 0.05f)
+                    bDirect = true;
+            }
+            else 
+            {
+                transform.position -= Speed;
+
+                if (transform.position.x > InitPos.x + 0.05f)
+                    bDirect = false;
+            }
+        }
+    }
+
+    // 当たった瞬間呼ばれる
+    void OnCollisionEnter(Collision hit)
+    {
+        Mold mold;
+        if (hit.gameObject.tag == "Mold")
+        {
+            mold = hit.gameObject.GetComponent<Mold>();
+            //mold.HitCollider(transform);
+            Destroy(transform.gameObject);
+        }
+    }
+}
---
+++ CESA2017/Assets/Script/sceneload.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class sceneload : MonoBehaviour
+{
+
+    public GameObject ImageObj;
+
+    void Update()
+    {
+        /*
+        if (ImageObj.GetComponent<fadeout>().alpha > 1)
+        {
+                SceneManager.LoadScene("game");
+        }
+         */
+
+
+        if (ImageObj.GetComponent<fadeout>().alpha > 1)
+        {
+            if (SceneManager.GetActiveScene().name == "title")
+            {
+                SceneManager.LoadScene("Test_Takahashi");
+            }
+
+            //    if (SceneManager.GetActiveScene().name == "game")
+            //    {
+            //        SceneManager.LoadScene("result");
+            //    } 
+
+            //   if (SceneManager.GetActiveScene().name == "result")
+            //    {
+            //        SceneManager.LoadScene("title");
+            //    }
+
+        }
+
+
+
+        if (Input.GetMouseButtonDown(0))
+        {
+
+            ImageObj.GetComponent<fadeout>().go = true;
+            //ImageObj.GetComponent<fadeout>().go2 = true;
+
+        }
+    }
+
+
+
+
+
+}
